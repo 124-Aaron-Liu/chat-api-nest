3615d226480aab6bbb456316ab6d5dd806306107
AaronLiu <aaron.liu@langlive.com>
2025-02-04 11:55:45 +0800

    feat: create service demo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { CatsController} from './cat/cat.controller';
+import { CatsController} from './cats/cats.controller';
+import {CatsService} from './cats/cats.service';
+
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
