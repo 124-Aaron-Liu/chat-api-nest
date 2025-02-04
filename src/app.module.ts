84ede6dfdf62c7b2efcf9a40182966b25ec32d6c
AaronLiu <aaron.liu@langlive.com>
2025-02-04 15:04:37 +0800

    feat: middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerGetMiddleware } from './middlewares/logger.get.middleware';
+
 import { AppController } from './app.controller';
-import { CatsController} from './cats/cats.controller';
-import {CatsService} from './cats/cats.service';
 
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerPostMiddleware } from './middlewares/logger.post.middleware';
+import { LoggerAllMiddleware } from './middlewares/logger.all.middleware';
+import { logger } from './middlewares/logger.functional.middleware'
 
 @Module({
   // 引用feature module
@@ -13,4 +16,13 @@ import { CatsModule } from './cats/cats.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      // multiple middleware, functional middleware
+      .apply(LoggerGetMiddleware, LoggerAllMiddleware, logger)
+      .forRoutes({path: 'cats', method: RequestMethod.GET})
+      .apply(LoggerPostMiddleware)
+      .forRoutes({path: 'cats', method: RequestMethod.POST});
+  }
+}
