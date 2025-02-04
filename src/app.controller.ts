bf63c5bd59fbbbd242eeb422e661495e6b2b15ae
AaronLiu <aaron.liu@langlive.com>
2025-02-04 12:25:19 +0800

    feat: feature module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 因為cats.module.ts有設定exports: [CatsService]，所以可以直接注入
+  constructor(private readonly appService: AppService, private readonly catsService: CatsService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('cats2')
+  getCats() {
+    return this.catsService.findAll();  // 使用 CatsService 的方法
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { CatsController} from './cats/cats.controller';
 import {CatsService} from './cats/cats.service';
 
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  // 引用feature module
+  imports: [CatsModule],
+
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
